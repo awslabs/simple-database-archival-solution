9bb0af2b694998c5002138c2b82eed07b99b16dc
Rui Costa <ruicos@amazon.com>
2024-12-17 20:46:35 -0500

    fix!: replace deprecated S3Origin with S3BucketOrigin in CloudFront distribution

--- deploy/src/constructs/cloudfront-s3-website-construct.ts
+++ deploy/src/constructs/cloudfront-s3-website-construct.ts
@@ -1,56 +1,27 @@
-/**
- * Copyright 2023 Amazon.com, Inc. and its affiliates. All Rights Reserved.
- *
- * Licensed under the Amazon Software License (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *   http://aws.amazon.com/asl/
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
 import * as cdk from 'aws-cdk-lib';
-import { iam } from 'cdk-nag/lib/rules';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as s3deployment from 'aws-cdk-lib/aws-s3-deployment';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { v4 as uuidv4 } from 'uuid';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+
+function getShortUuidSegment(): string {
+	const fullUuid = uuidv4();
+	return fullUuid.split('-')[0];
+}
 
 export interface CloudFrontS3WebSiteConstructProps extends cdk.StackProps {
-	/**
-	 * The path to the build directory of the web site, relative to the project root
-	 * ex: "./app/build"
-	 */
 	readonly webSiteBuildPath: string;
-
-	/**
-	 * The Arn of the WafV2 WebAcl.
-	 */
 	readonly webAclArn?: string;
 }
 
-const defaultProps: Partial<CloudFrontS3WebSiteConstructProps> = {};
-
-/**
- * Deploys a CloudFront Distribution pointing to an S3 bucket containing the deployed web application {webSiteBuildPath}.
- * Creates:
- * - S3 bucket
- * - CloudFrontDistribution
- * - OriginAccessIdentity
- *
- * On redeployment, will automatically invalidate the CloudFront distribution cache
- */
 export class CloudFrontS3WebSiteConstruct extends Construct {
 	/**
-	 * The origin access identity used to access the S3 website
+	 * The CloudFront distribution for the website
 	 */
-	public originAccessIdentity: cdk.aws_cloudfront.OriginAccessIdentity;
-
-	/**
-	 * The cloud front distribution to attach additional behaviors like `/api`
-	 */
-	public cloudFrontDistribution: cdk.aws_cloudfront.Distribution;
+	public readonly cloudFrontDistribution: cloudfront.Distribution;
 
 	constructor(
 		parent: Construct,
@@ -59,112 +30,89 @@ export class CloudFrontS3WebSiteConstruct extends Construct {
 	) {
 		super(parent, name);
 
-		props = { ...defaultProps, ...props };
-
-		// When using Distribution, do not set the s3 bucket website documents
-		// if these are set then the distribution origin is configured for HTTP communication with the
-		// s3 bucket and won't configure the cloudformation correctly.
-
-		const siteBucket = new cdk.aws_s3.Bucket(this, 'WebApp', {
-			encryption: cdk.aws_s3.BucketEncryption.S3_MANAGED,
+		const siteBucket = new s3.Bucket(this, 'WebApp', {
+			bucketName: `webapp-${getShortUuidSegment()}`,
+			encryption: s3.BucketEncryption.S3_MANAGED,
 			autoDeleteObjects: true,
-			blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ALL,
+			blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
 			removalPolicy: cdk.RemovalPolicy.DESTROY,
-			serverAccessLogsBucket: new cdk.aws_s3.Bucket(
-				this,
-				'WebAppServerAccessLogs',
-				{
-					encryption: cdk.aws_s3.BucketEncryption.S3_MANAGED,
-					autoDeleteObjects: true,
-					blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ALL,
-					removalPolicy: cdk.RemovalPolicy.DESTROY,
-					serverAccessLogsPrefix: 'access-logs',
-					enforceSSL: true,
-				}
-			),
+			enforceSSL: true,
+			serverAccessLogsPrefix: '/access-logs/',
+			versioned: true,
 		});
 
+		// Enforce HTTPS-only access
 		siteBucket.addToResourcePolicy(
-			new cdk.aws_iam.PolicyStatement({
+			new iam.PolicyStatement({
 				sid: 'EnforceTLS',
-				effect: cdk.aws_iam.Effect.DENY,
-				principals: [new cdk.aws_iam.AnyPrincipal()],
+				effect: iam.Effect.DENY,
+				principals: [new iam.AnyPrincipal()],
 				actions: ['s3:*'],
-				resources: [siteBucket.bucketArn, siteBucket.bucketArn + '/*'],
+				resources: [siteBucket.bucketArn, `${siteBucket.bucketArn}/*`],
 				conditions: { Bool: { 'aws:SecureTransport': 'false' } },
 			})
 		);
 
-		const originAccessIdentity =
-			new cdk.aws_cloudfront.OriginAccessIdentity(
-				this,
-				'OriginAccessIdentity'
-			);
-		siteBucket.grantRead(originAccessIdentity);
-
-		const s3origin = new cdk.aws_cloudfront_origins.S3Origin(siteBucket, {
-			originAccessIdentity: originAccessIdentity,
+		// Create Origin Access Identity (OAI)
+		const oai = new cloudfront.OriginAccessIdentity(this, 'OAI', {
+			comment: 'OAI for CloudFront to access S3 bucket securely',
 		});
 
-		const cloudFrontDistribution = new cdk.aws_cloudfront.Distribution(
+		// Grant CloudFront read access to the S3 bucket
+		siteBucket.grantRead(oai);
+
+		// Define the CloudFront Distribution
+		this.cloudFrontDistribution = new cloudfront.Distribution(
 			this,
 			'WebAppDistribution',
 			{
 				defaultBehavior: {
-					origin: s3origin,
-					cachePolicy: new cdk.aws_cloudfront.CachePolicy(
+					origin: origins.S3BucketOrigin.withOriginAccessControl(
+						siteBucket
+					),
+					cachePolicy: new cloudfront.CachePolicy(
 						this,
 						'CachePolicy',
 						{
 							defaultTtl: cdk.Duration.hours(1),
+							minTtl: cdk.Duration.minutes(1),
+							maxTtl: cdk.Duration.days(1),
+							enableAcceptEncodingGzip: true,
+							enableAcceptEncodingBrotli: true,
 						}
 					),
-					allowedMethods: cdk.aws_cloudfront.AllowedMethods.ALLOW_ALL,
+					allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
 					viewerProtocolPolicy:
-						cdk.aws_cloudfront.ViewerProtocolPolicy.HTTPS_ONLY,
+						cloudfront.ViewerProtocolPolicy.HTTPS_ONLY,
 				},
 				errorResponses: [
 					{
 						httpStatus: 404,
-						ttl: cdk.Duration.hours(0),
 						responseHttpStatus: 200,
 						responsePagePath: '/index.html',
+						ttl: cdk.Duration.minutes(30),
 					},
 				],
 				defaultRootObject: 'index.html',
 				webAclId: props.webAclArn,
 				minimumProtocolVersion:
-					cdk.aws_cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021, // Required by security
+					cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
 			}
 		);
 
-		new cdk.aws_s3_deployment.BucketDeployment(
-			this,
-			'DeployWithInvalidation',
-			{
-				sources: [
-					cdk.aws_s3_deployment.Source.asset(props.webSiteBuildPath),
-				], // from root directory
-				destinationBucket: siteBucket,
-				distribution: cloudFrontDistribution, // this assignment, on redeploy, will automatically invalidate the cloudfront cache
-				distributionPaths: ['/*'],
-				// default of 128 isn't large enough for larger website deployments. More memory doesn't improve the performance.
-				// You want just enough memory to guarantee deployment
-				memoryLimit: 512,
-			}
-		);
-
-		// export any cf outputs
-		new cdk.CfnOutput(this, 'SiteBucket', { value: siteBucket.bucketName });
-		new cdk.CfnOutput(this, 'CloudFrontDistributionId', {
-			value: cloudFrontDistribution.distributionId,
+		// Deploy website assets to S3 bucket
+		new s3deployment.BucketDeployment(this, 'DeployWithInvalidation', {
+			sources: [s3deployment.Source.asset(props.webSiteBuildPath)],
+			destinationBucket: siteBucket,
+			distribution: this.cloudFrontDistribution,
+			distributionPaths: ['/*'],
 		});
+
 		new cdk.CfnOutput(this, 'CloudFrontDistributionDomainName', {
-			value: cloudFrontDistribution.distributionDomainName,
+			value: this.cloudFrontDistribution.distributionDomainName,
 		});
-
-		// assign public properties
-		this.originAccessIdentity = originAccessIdentity;
-		this.cloudFrontDistribution = cloudFrontDistribution;
+		new cdk.CfnOutput(this, 'SiteBucketName', {
+			value: siteBucket.bucketName,
+		});
 	}
 }
